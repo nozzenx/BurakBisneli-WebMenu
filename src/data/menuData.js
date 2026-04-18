eaeb2bade90afceec12031baa1f7c1053d054733
Burak Bisneli <bisneliburak@gmail.com>
2026-04-19 01:10:04 +0300

    adding more dummy data

--- src/data/menuData.js
+++ src/data/menuData.js
@@ -1,10 +1,24 @@
 import Chicken from '../assets/roastedChickenImage.jpg'
 import Steak from '../assets/ribeyeSteak.jpg'
+import IzgaraKofte from '../assets/izgaraKofte.jpg'
+import KuzuTandir from '../assets/kuzuTandir.jpg'
+import MercimekCorba from '../assets/mercimekCorba.jpg'
+import Lahmacun from '../assets/lahmacun.jpg'
+import HunkarBegendi from '../assets/hunkarBegendi.jpg'
+import Pide from '../assets/pide.jpg'
+import Baklava from '../assets/Baklava.png'
 
 
 const menuData = [
-    {id: 1, name: "Tavuk", info: "Firinda pismis, soslu, lezzetli bir tavuk.", imageUrl: Chicken },
-    {id: 2, name: "Et", info: "Ozel sosta mukemmel kivamda pismis bir et.", imageUrl: Steak }
+    { id: 1, name: "Tavuk", info: "Fırında pişmiş, soslu, lezzetli bir tavuk.", imageUrl: Chicken },
+    { id: 2, name: "Et", info: "Özel sosta mükemmel kıvamda pişmiş bir et.", imageUrl: Steak },
+    { id: 3, name: "Izgara Köfte", info: "El yapımı baharatlı köfte, közlenmiş sebzelerle sergilenir.", imageUrl: IzgaraKofte },
+    { id: 4, name: "Kuzu Tandır", info: "Saatlerce fırında pişirilmiş, kemikten düşen yumuşaklıkta kuzu.", imageUrl: KuzuTandir },
+    { id: 5, name: "Mercimek Çorbası", info: "Geleneksel tarifte pişirilmiş, nane yağıyla servis edilen çorba.", imageUrl: MercimekCorba },
+    { id: 6, name: "Lahmacun", info: "İnce hamur üzerine kıymalı harç, limon ve maydanozla sunulur.", imageUrl: Lahmacun },
+    { id: 7, name: "Hünkar Beğendi", info: "Közlenmiş patlıcan püresi üzerinde yumuşacık kuzu güveci.", imageUrl: HunkarBegendi },
+    { id: 8, name: "Pide", info: "Taş fırında pişirilmiş, kıymalı ve kaşarlı geleneksel Türk pidesi.", imageUrl: Pide },
+    { id: 9, name: "Baklava", info: "Kat kat yufka arasında antep fıstığı, gül suyu şerbeti ile.", imageUrl: Baklava }
 ]
 
 export default menuData
